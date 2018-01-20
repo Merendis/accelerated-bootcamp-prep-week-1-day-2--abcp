9bc01c9216ecb48ef9a998eb71bed4e63b9de1ad
Meredith D Doucette <meredith.doucette@gmail.com>
2018-01-20 23:11:34 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -18,7 +18,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-
+  return
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
